827d512800a1401da5c377096c0047da907448d0
Javier <javier_bislip@hotmail.com>
2019-11-06 00:22:33 -0400

    First commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const PORT = 3000 || process.env.PORT;
+
 const express = require("express");
 const path = require("path");
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
